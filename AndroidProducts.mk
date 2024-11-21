b7e1d2cd90516f7f5a1c8712a0d29cdc70af9713
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-11-21 09:42:31 -0800

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_a22X.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a22X-user \
+    lineage_a22X-userdebug \
+    lineage_a22X-eng
